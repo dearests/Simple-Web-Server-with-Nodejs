c1197482b6e08c80c04d7c362a688c422954576a
dearests <soguazu@gmail.com>
2017-07-31 14:22:17 +0100

    adding route

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
-var http = require('http')
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+//Array of Mime Types
+
+var mimeType = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+
+//Create Server
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.write("Up and running....")
-    res.end();
-}).listen(8080);
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri))
 
-console.log("Server running on port 8080");
+    console.log("Loading "+uri);
+    var stats;
+
+    try {
+        stats = fs.lststSync(fileName);
+        console.log(stats)
+    } catch(fileName) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+}).listen(8080);
