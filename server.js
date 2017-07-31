46f7d15a3282f1a4b4be95ec80aff5f6d758516e
dearests <soguazu@gmail.com>
2017-07-31 12:09:37 +0100

    printing to stdout

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http')
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type':'text/plain'});
+    res.write("Up and running....")
     res.end();
 }).listen(8080);
 
