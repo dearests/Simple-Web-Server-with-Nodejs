8ed7cf55881d5bf6c4f5ce20c5f11e43e446ee27
dearests <soguazu@gmail.com>
2017-07-31 15:19:05 +0100

    error correction

--- server.js
+++ server.js
@@ -19,14 +19,12 @@ var mimeTypes = {
 
 http.createServer(function (req, res) {
     var uri = url.parse(req.url).pathname;
-    var fileName = path.join(process.cwd(), unescape(uri))
-
-    console.log("Loading "+uri);
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log("Loading"+uri);
     var stats;
 
     try {
-        stats = fs.lststSync(fileName);
-        console.log(stats)
+        stats = fs.lstatSync(fileName);
     } catch(e) {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.write('404 Not Found\n');
